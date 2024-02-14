b9eb9d339a3793255c1831273d70b67faebde698
michele <michelepizzeghello@gmail.com>
2024-02-14 16:05:16 +0100

    Aggiunte tabs

--- lib/main.dart
+++ lib/main.dart
@@ -79,9 +79,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<void> signOut() async{
-    await Auth().signOut();
-  }
 
 
   @override
@@ -96,32 +93,11 @@ class _MyHomePageState extends State<MyHomePage> {
     context.read<UserProvider>().setPunti(punteggio);
     context.read<UserProvider>().setLivello(livello);
     //"Valore ottenuto da Firebase: $punteggio"
-
-
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        actions: [
-          IconButton(onPressed: (){
-            signOut();
-          }, icon: Icon(Icons.logout))
-        ],
-        
-        title: Text(widget.title),
-      ),
-      body: Widget028(),
-    );
+    return const Widget028();
   }
 }
 
--- lib/screens/QuestScreen.dart
+++ lib/screens/QuestScreen.dart
@@ -10,6 +10,32 @@ class _QuestScreenState extends State<QuestScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ListQuest();
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Quest Planner'),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          bottom: const TabBar(
+            isScrollable: true,
+            labelPadding: EdgeInsets.all(10),
+            tabAlignment: TabAlignment.start,
+            tabs: [
+              Tab(child: Text('Tutti', style: TextStyle(fontSize: 22),)),
+              Tab(child: Text('Urgente', style: TextStyle(fontSize: 22),)),
+              Tab(child: Text('Prioritario', style: TextStyle(fontSize: 22),)),
+              Tab(child: Text('Secondario', style: TextStyle(fontSize: 22),)),
+              Tab(child: Text('Inbox', style: TextStyle(fontSize: 22),)),
+          ],),
+        ),
+        body: const TabBarView(children: [
+          ListQuest(),
+          ListQuest(),
+          ListQuest(),
+          ListQuest(),
+          ListQuest(),
+        ])
+      ),
+    );
   }
 }
